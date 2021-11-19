ab37a17bdc86118378477e8348bcc4bbd9747030
praveenvenkat1 <pv2003@hw.ac.uk>
2021-11-19 23:21:11 +0400

    Commit

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require('express');
 const app = express();
 
 //send an HTTP response when receiving HTTP GET /
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.send("Hello, your server is up and running");
+    res.sendFile("public/index.html",{root: __dirname});
 });
 
 //make the app listen on port 
-const port = process.argv[2] || process.env.PORT || 3000;
+const port = process.argv[2] || process.env.PORT || 3001;
 const server = app.listen(port, () => {
     console.log(`Cart app listening at http://localhost:${port}`);
 })
