5de4f928a25aa3d84ebbf26a91231beef9712095
praveenvenkat1 <pv2003@hw.ac.uk>
2021-11-21 21:25:46 +0400

    Commit

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.get('/', (req, res) => {
 
 
 //handling static HTML and EJS templates
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index'); //no need for ejs extension
@@ -22,8 +21,13 @@ app.get('/contacts', (req, res) => {
     res.render('contacts'); 
    });
 
+//pass requests to the router middleware
+const router = require('./routes/post');
+app.use(router);
+
 //make the app listen on port 
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Cart app listening at http://localhost:${port}`);
 });
+
