b6e242a319c778e78d570c50fc6107f5cf6282fa
praveenvenkat1 <pv2003@hw.ac.uk>
2021-11-22 22:56:57 +0400

    Commit

--- db/dbQuery.js
+++ db/dbQuery.js
@@ -3,7 +3,7 @@ const databasename = "sales";
 
 var pool = mysql.createPool({
     connectionLimit: 100,
-    host: 'localhost',
+    host: '127.0.0.1',
     user: "root",
     password: "",
     database: "sales",
--- routes/apis.js
+++ routes/apis.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const clientController = require('../controllers/clientController');
 
 //define a router and create routes
 const router = express.Router();
@@ -13,9 +14,9 @@ router.get('/api/article/:id', productController.getProductByID);
 //routes for dynamic processing of products
 //-----------------------------------------------
 //route for registration
-
+router.post('/api/register', clientController.registerControl);
 //route for login
-
+router.post('/api/login', clientController.loginControl);
 
 //export router
 module.exports = router;
